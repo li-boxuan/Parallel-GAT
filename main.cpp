985d565c0d6ab7c2d754d74faed65b753940a935
Boxuan Li <liboxuan@connect.hku.hk>
2022-04-16 10:51:20 -0400

    Read trained parameters from file

--- main.cpp
+++ main.cpp
@@ -14,12 +14,34 @@ int main() {
     nodes[i] = new node(feat_dim, num_heads, msg_dim);
     nodes[i]->random_init();
   }
+
+  std::ifstream in("models/gat_ppi_model.txt");
+  if (!in) {
+    std::cout << "Cannot open models/gat_ppi_model.txt\n";
+    return 1;
+  }
+
   GAT gat_1 = GAT(num_nodes, num_heads, feat_dim, msg_dim);
-  GAT gat_2 = GAT(num_nodes, num_heads, feat_dim, msg_dim);
-  gat_1.random_init();
-  gat_2.random_init();
+  for (int i = 0; i < num_heads; i++) {
+    for (int j = 0; j < msg_dim; j++) {
+      in >> gat_1.params[i]->A1[j];
+    }
+  }
+  for (int i = 0; i < num_heads; i++) {
+    for (int j = 0; j < msg_dim; j++) {
+      in >> gat_1.params[i]->A2[j];
+    }
+  }
+  for (int i = 0; i < num_heads; i++) {
+    for (int k = 0; k < msg_dim; k++) {
+      for (int j = 0; j < feat_dim; j++) {
+        in >> gat_1.params[i]->W[k][j];
+      }
+    }
+  }
+  in.close();
   gat_1.forward(nodes, &adj);
-  gat_2.forward(nodes, &adj);
+
   for (int i = 0; i < num_nodes; i++) {
     delete (nodes[i]);
   }
