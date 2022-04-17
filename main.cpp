c377125fce28c70e5661d3b5186ca3c24d05b93b
Boxuan Li <liboxuan@connect.hku.hk>
2022-04-16 23:54:58 -0400

    Fix model load

--- main.cpp
+++ main.cpp
@@ -37,12 +37,12 @@ int main() {
   GAT gat_1 = GAT(num_nodes, num_heads, feat_dim, msg_dim);
   for (int i = 0; i < num_heads; i++) {
     for (int j = 0; j < msg_dim; j++) {
-      in >> gat_1.params[i]->A1[j];
+      in >> gat_1.params[i]->A2[j];
     }
   }
   for (int i = 0; i < num_heads; i++) {
     for (int j = 0; j < msg_dim; j++) {
-      in >> gat_1.params[i]->A2[j];
+      in >> gat_1.params[i]->A1[j];
     }
   }
   for (int i = 0; i < num_heads; i++) {
